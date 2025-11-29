2106b4c0b57ca416b93a7553f0ff6b641ef7a20d
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-11-30 03:20:36 +0900

    fix: solve backend bug

--- frontend/src/routes/login.tsx
+++ frontend/src/routes/login.tsx
@@ -1,17 +1,33 @@
 import { createFileRoute } from '@tanstack/react-router'
 import logoSvg from '../assets/logo.svg'
 
+
 export const Route = createFileRoute('/login')({
   component: RouteComponent,
 })
 
+export const getApiUrl = () => {
+  return import.meta.env.VITE_API_BASE_URL || `http://${window.location.hostname}:3000`;
+};
+
 function RouteComponent() {
-  return <div className="flex flex-col gap-[30px] items-center justify-center h-screen">
+  const handleLogin = () => {
+    const apiUrl = getApiUrl();
+    const redirectUrl = encodeURIComponent(
+      `${window.location.pathname}${window.location.search}${window.location.hash}` || '/',
+    );
+    window.location.href = `${apiUrl}/auth/login?redirect_url=${redirectUrl}`;
+  };
+
+  return (
+  <div className="flex flex-col gap-[30px] items-center justify-center h-screen">
     <img src={logoSvg} alt="logo" className="w-[300px] h-auto" />
     <button 
         className="py-[7px] px-[20px] font-bold text-medium justify-center items-center gap-[10px] rounded-[10px] border-[1px] border-black"
+        onClick={handleLogin}
     >
-        Login with GIST account
+      Login with GIST account
     </button>
-    </div>
+  </div>
+  )
 }
