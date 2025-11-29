c077a62b15c2ec85d31ea0ed34b235d9573974eb
Suyeon06 <suyeonchae0919@gm.gist.ac.kr>
2025-11-29 19:00:10 +0900

    design: update findUser page

--- frontend/src/routes/findUser.tsx
+++ frontend/src/routes/findUser.tsx
@@ -1,4 +1,6 @@
 import { createFileRoute, Link } from '@tanstack/react-router'
+import logoSvg from '../assets/logo.svg'
+import searchSvg from '../assets/search.svg'
 
 export const Route = createFileRoute('/findUser')({
   component: RouteComponent,
@@ -30,7 +32,13 @@ function RouteComponent() {
           <span className="font-medium">방으로 돌아가기</span>
         </Link>
       </div>
-      <div className="flex-1 overflow-auto p-6 bg-[#FFF6E9] relative">content</div>
+      <div className="flex flex-col gap-[30px] items-center justify-center h-screen bg-white">
+        <img src={logoSvg} alt="logo" className="w-[300px] h-auto" />
+        <div className="flex border-[1px] border-black w-auto gap-[30px] py-[5px] px-[30px] justify-between items-center rounded-[10px] bg-white">
+          <span className="font-semibold text-md text-[#D9D9D9]">친구의 이름을 입력해주세요. ex) 25 홍길동</span>
+          <img src={searchSvg} alt="search" className="w-[30px] h-auto" />
+        </div>
+      </div>
     </div>
   )
 }
